c666d21c0fa898b4c84e0af3325523e66fbc4f43
Frank Supper <frank.supper@gmail.com>
2015-02-16 20:26:19 +0100

    Last touch on P5

--- Solutions/Problem005.cs
+++ Solutions/Problem005.cs
@@ -12,7 +12,7 @@
 		public override long Solution()
 		{
 			int number = 20;
-			while (number % 20 != 0 || number % 19 != 0 || number % 18 != 0 || number % 17 != 0 || number % 16 != 0 ||
+			while (number % 19 != 0 || number % 18 != 0 || number % 17 != 0 || number % 16 != 0 ||
 				number % 15 != 0 || number % 14 != 0 || number % 13 != 0 || number % 12 != 0 || number % 11 != 0)
 			{
 				number += 20;
